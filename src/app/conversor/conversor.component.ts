7577f7d2287ecc47c078b45fc9900c90e465f4c9
josusz <josuhcarvalho@gmail.com>
2023-04-11 10:46:28 -0300

    estilização e ajuste de script

--- src/app/conversor/conversor.component.ts
+++ src/app/conversor/conversor.component.ts
@@ -7,19 +7,20 @@ import { Component } from '@angular/core';
 })
 export class ConversorComponent{
   valor: number=0;
+  valorCambio: number=0;
   valorConvertido: number=0;
-  moedaOrigem: string="";
-  moedaDestino: string="";
+  moedaOrigem: string="real";
+  moedaDestino: string="dolar";
 
   converter(){
     if (this.moedaOrigem === "real" && this.moedaDestino === "dolar") {
-      this.valorConvertido = this.valor / 5.5;
+      this.valorConvertido = this.valor / this.valorCambio;
     } else if (this.moedaOrigem === "real" && this.moedaDestino === "euro") {
-      this.valorConvertido = this.valor / 6.2;
+      this.valorConvertido = this.valor / this.valorCambio;
     } else if (this.moedaOrigem === "dolar" && this.moedaDestino === "real") {
-      this.valorConvertido = this.valor * 5.5;
+      this.valorConvertido = this.valor * this.valorCambio;
     } else if (this.moedaOrigem === "euro" && this.moedaDestino === "real") {
-      this.valorConvertido = this.valor * 6.2;
+      this.valorConvertido = this.valor * this.valorCambio;
     } else {
       this.valorConvertido = this.valor;
     }
